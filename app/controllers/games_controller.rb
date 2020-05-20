10c5df70f8930e948cc4b8b05248172ad4895167
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-05-20 16:04:02 -0500

    add destroy method to games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -18,6 +18,12 @@ class GamesController < ApplicationController
     end
   end
 
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+    render json: { status: 'Successfully destroyed', data: @game }, status: :ok
+  end
+
   private
   def game_params
     params.require(:game).permit(:name, :genre, :price, :release_date, :company_id)
