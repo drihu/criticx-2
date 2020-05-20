d2197ecdfdb2853c92ccc2dbffd3bb322dac8690
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-05-20 11:04:00 -0500

    add resources to routes

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,4 +3,9 @@ class CompaniesController < ApplicationController
     @companies = Company.all
     render json: @companies
   end
+
+  def show
+    @company = Company.find(params[:id])
+    render json: @company
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/companies', to: 'companies#index'
-  get '/games', to: 'games#index'
+  resources :companies
+  resources :games
 end
