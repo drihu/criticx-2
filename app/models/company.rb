435bde688f8fd8ec6e3d0d8ac7ebe7d456905b7e
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-05-20 00:26:26 -0500

    add validations to company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,5 @@
 class Company < ApplicationRecord
   has_many :games
+  validates :name, presence: true, uniqueness: true, length: { maximum: 40 }
+  validates :country, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Game.create(
+Company.create!(
+  name: 'Valve',
+  description: 'Creator of Dota 2 and Underlords',
+  start_date: '2005-12-14',
+  country: 'EE.UU.',
+)
+
+Game.create!(
   name: 'Rune Factory 4 Special',
   genre: 'Role-playing (RPG)',
   price: 5999,
   release_date: '2019-07-25',
+  company_id: 1,
 )
 
 game = Game.new(
@@ -17,15 +25,16 @@ game = Game.new(
   genre: 'Simulator',
   price: 5999,
   release_date: '2019-09-13',
+  company_id: 1,
 )
 game.save
 
 games = [
-  {:name=>"Zombie Driver: Immortal Edition", :genre=>"Racing", :price=>5999, :release_date=>"2019-Jul-25"},
-  {:name=>"Remnant: From the Ashes", :genre=>"Shooter", :price=>6999, :release_date=>"2019-Aug-20"},
-  {:name=>"Monster Hunter: World - Iceborne", :genre=>"Adventure", :price=>6999, :release_date=>"2019-Sep-06"},
-  {:name=>"Cyberpunk 2077", :genre=>"Role-playing (RPG)", :price=>4999, :release_date=>"2020-Sep-17"},
-  {:name=>"Pokémon Shield", :genre=>"Role-playing (RPG)", :price=>5999, :release_date=>"2019-Nov-15"},
-  {:name=>"Children of Morta", :genre=>"Role-playing (RPG)", :price=>6999, :release_date=>"2019-Sep-03"},
+  {:name=>"Zombie Driver: Immortal Edition", :genre=>"Racing", :price=>5999, :release_date=>"2019-Jul-25", :company_id=>1},
+  {:name=>"Remnant: From the Ashes", :genre=>"Shooter", :price=>6999, :release_date=>"2019-Aug-20", :company_id=>1},
+  {:name=>"Monster Hunter: World - Iceborne", :genre=>"Adventure", :price=>6999, :release_date=>"2019-Sep-06", :company_id=>1},
+  {:name=>"Cyberpunk 2077", :genre=>"Role-playing (RPG)", :price=>4999, :release_date=>"2020-Sep-17", :company_id=>1},
+  {:name=>"Pokémon Shield", :genre=>"Role-playing (RPG)", :price=>5999, :release_date=>"2019-Nov-15", :company_id=>1},
+  {:name=>"Children of Morta", :genre=>"Role-playing (RPG)", :price=>6999, :release_date=>"2019-Sep-03", :company_id=>1},
 ]
-games.each { |game| Game.create(game) }
+games.each { |game| Game.create!(game) }
