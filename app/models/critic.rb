5cff446e0a6ed993fdef7c14eabd61c6c6004468
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-05-20 00:48:06 -0500

    add validations to critic model

--- app/models/critic.rb
+++ app/models/critic.rb
@@ -1,3 +1,6 @@
 class Critic < ApplicationRecord
   belongs_to :game
+  validates :username, presence: true
+  validates :username, format: { with: /\A[A-Za-z0-9]+\z/ }
+  validates :body, :title, presence: { message: 'Please write something!' }
 end
