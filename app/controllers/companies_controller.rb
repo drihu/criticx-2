320f2295b8fcb574e63952f90cc36ce2d8d75dfb
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-05-20 16:45:14 -0500

    add update method to companies controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,8 +4,22 @@ class CompaniesController < ApplicationController
     render json: @companies
   end
 
+  def update
+    @company = Company.find(params[:id])
+    if @company.update_attributes(company_params)
+      render json: @company
+    else
+      render json: @company.errors, status: :unprocessable_entity
+    end
+  end
+
   def show
     @company = Company.find(params[:id])
     render json: @company
+  end
+
+  private
+  def company_params
+    params.require(:company).permit(:name, :description, :start_date, :country)
   end
 end
